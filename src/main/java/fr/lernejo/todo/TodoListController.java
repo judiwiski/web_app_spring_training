b2c2e36f8666606b677b24e65a0ea531b8c7fe8a
VAHE <vahe@et.esiea.fr>
2023-02-14 11:16:10 +0100

    part7

--- src/main/java/fr/lernejo/todo/TodoListController.java
+++ src/main/java/fr/lernejo/todo/TodoListController.java
@@ -3,11 +3,35 @@ package fr.lernejo.todo;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
+import java.util.List;
 
 @RestController
 public class TodoListController {
 
-    private ArrayList<Todo> list = new ArrayList<>();
+    private TodoRepository repository;
+    public TodoListController(TodoRepository repository){
+        this.repository = repository;
+    }
+
+    @PostMapping("/api/todo")
+    public void add(@RequestBody Todo newElement){
+        TodoEntity entity = new TodoEntity();
+        entity.message = newElement.message();
+        entity.author = newElement.author();
+        repository.save(entity);
+    }
+
+    @GetMapping("/api/todo")
+    public List<Todo> getTodoList() {
+        ArrayList<Todo> todos = new ArrayList<>();
+         for (TodoEntity todoEntity : repository.findAll()){
+             Todo todo = new Todo(todoEntity.message, todoEntity.author);
+             todos.add(todo);
+         }
+         return  todos;
+    }
+
+   /* private ArrayList<Todo> list = new ArrayList<>();
 
     @PostMapping//("/api/todo")
     public void add(@RequestBody Todo newElement){
@@ -17,5 +41,5 @@ public class TodoListController {
     @GetMapping//("/api/todo")
     public ArrayList<Todo> getTodoList(){
         return list;
-    }
+    }*/
 }
